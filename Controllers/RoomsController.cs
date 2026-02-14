06f5caba1b0290615620f87a22ea8c65aaa480cd
Fatimah Azzahra <120361765+ftazzahra@users.noreply.github.com>
2026-02-14 15:23:40 +0700

    feat(room): fix create room include location and availability

--- Controllers/RoomsController.cs
+++ Controllers/RoomsController.cs
@@ -43,7 +43,9 @@ namespace erooms.Controllers
             var room = new Room
             {
                 Name = dto.Name,
-                Capacity = dto.Capacity
+                Location = dto.Location,          
+                Capacity = dto.Capacity,
+                IsAvailable = dto.IsAvailable     
             };
 
             _context.Rooms.Add(room);
